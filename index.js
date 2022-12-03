04a2f1ae90b807f74a547ec99b46cd53a07dbcb6
Nisha Karki <karkinisha536@gmail.com>
2022-12-03 18:33:59 +0545

    second changes

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function fun() {
+const name= document.getElementsByClassName(".data name").value;
+const email= document.getElementsByClassName(".data email").value;
+const message= document.getElementsByClassName(".data message").value;
+const subject= document.getElementsByClassName(".data subject").value;
+
+
+    alert ("name:" + name);
+ }
